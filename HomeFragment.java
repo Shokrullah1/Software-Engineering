09579f777333ac9302211601ccbca906af57e1d8
Shokrullah1 <81216368+Shokrullah1@users.noreply.github.com>
2021-04-28 20:22:32 +0430

    Add files via upload

---
+++ HomeFragment.java
@@ -0,0 +1,25 @@
+package com.example.dsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+    private Button button;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        return v;
+    }
+
+}
+
